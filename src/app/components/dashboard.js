f920b012c55021478c07c7f6b54151f29bfb6c3b
Niveditha Karmegam <kniveditha777@gmail.com>
2018-02-22 10:01:09 +0530

    Dashboard updated to show slowest tests

--- src/app/components/dashboard.js
+++ src/app/components/dashboard.js
@@ -10,7 +10,7 @@ class Dashboard extends Component {
                         <div className="align-inline">
                             <iframe width="520" height="290" seamless frameBorder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSj1ytWaSU6ng28bHJ7OcJtjbC2mIqVbVURFm1uc54czef0OYfy2J05dDx-c2LAjxwlnR2EfmzVG6j9/pubchart?oid=1340983100&amp;format=interactive"></iframe>
                             <iframe width="470" height="290" seamless frameBorder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSj1ytWaSU6ng28bHJ7OcJtjbC2mIqVbVURFm1uc54czef0OYfy2J05dDx-c2LAjxwlnR2EfmzVG6j9/pubchart?oid=1079753516&amp;format=interactive"></iframe>
-                            <iframe width="220" height="290" seamless frameBorder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSj1ytWaSU6ng28bHJ7OcJtjbC2mIqVbVURFm1uc54czef0OYfy2J05dDx-c2LAjxwlnR2EfmzVG6j9/pubchart?oid=600883071&amp;format=interactive"></iframe>
+                            <iframe width="296.5" height="290" seamless frameBorder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSj1ytWaSU6ng28bHJ7OcJtjbC2mIqVbVURFm1uc54czef0OYfy2J05dDx-c2LAjxwlnR2EfmzVG6j9/pubchart?oid=474246580&amp;format=interactive"></iframe>
                         </div>
                     </center>
                 </Panel>
@@ -20,7 +20,7 @@ class Dashboard extends Component {
                         <div className="align-inline">
                             <iframe width="520" height="290" seamless frameBorder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSj1ytWaSU6ng28bHJ7OcJtjbC2mIqVbVURFm1uc54czef0OYfy2J05dDx-c2LAjxwlnR2EfmzVG6j9/pubchart?oid=1340983100&amp;format=interactive"></iframe>
                             <iframe width="470" height="290" seamless frameBorder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSj1ytWaSU6ng28bHJ7OcJtjbC2mIqVbVURFm1uc54czef0OYfy2J05dDx-c2LAjxwlnR2EfmzVG6j9/pubchart?oid=872314850&amp;format=interactive"></iframe>
-                            <iframe width="220" height="200" seamless frameBorder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSj1ytWaSU6ng28bHJ7OcJtjbC2mIqVbVURFm1uc54czef0OYfy2J05dDx-c2LAjxwlnR2EfmzVG6j9/pubchart?oid=672451016&amp;format=interactive"></iframe>
+                            <iframe width="295" height="290" seamless frameBorder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSj1ytWaSU6ng28bHJ7OcJtjbC2mIqVbVURFm1uc54czef0OYfy2J05dDx-c2LAjxwlnR2EfmzVG6j9/pubchart?oid=1009737518&amp;format=interactive"></iframe>
                         </div>
                     </center>
                 </Panel>
@@ -31,6 +31,3 @@ class Dashboard extends Component {
 }
 
 export default Dashboard;
- /* 
- <iframe width="161.72506738544473" height="100" seamless frameBorder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSj1ytWaSU6ng28bHJ7OcJtjbC2mIqVbVURFm1uc54czef0OYfy2J05dDx-c2LAjxwlnR2EfmzVG6j9/pubchart?oid=1862890372&amp;format=interactive"></iframe>
- */
